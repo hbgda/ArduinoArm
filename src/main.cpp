d3e58634f8aa2d68cdd5902d53150d0460ceef53
hbgda <leroy.d275@gmail.com>
2021-11-10 04:21:00 +0000

    concept code

--- src/main.cpp
+++ src/main.cpp
@@ -14,11 +14,14 @@ int hingeControl = A2;
 int grabberControl = A3;
 
 // Default angle for servos
-int angle = 90; 
-int angle1 = 90;
-int angle2 = 90;
-int angle3 = 90;
-int angleStep = 5;
+int defaultAngle = 90;
+
+int baseCurr = 90;
+int baseHingeCurr = 90;
+int hingeCurr = 90;
+int grabberCurr = 0;
+
+bool shouldUseKeyboard = false;
 
 void setup() {
 
@@ -30,46 +33,108 @@ void setup() {
     grabberServo.attach(13);
   
     // Set servo roations to a default of 90 degrees
-    baseServo.write(angle);
-    baseHingeServo.write(angle1);
-    hingeServo.write(angle2); 
-    grabberServo.write(angle3); 
+    baseServo.write(defaultAngle);
+    baseHingeServo.write(defaultAngle);
+    hingeServo.write(defaultAngle); 
+    grabberServo.write(0); 
     
 }
 
-void loop() {
-
-    // Control Code
-    int baseVal = analogRead(baseControl);
-    float baseSetVal = map(baseVal, 0, 1023, 0, 180);
-    baseServo.write(baseSetVal);
-    delay(15);     
-
-    int baseHingeVal = analogRead(baseHingeControl);
-    float baseHingeSetVal = map(baseHingeVal, 0, 1023, 0, 180);
-    baseHingeServo.write(baseHingeSetVal);
-    delay(15);     
-
-    int hingeVal = analogRead(hingeControl);
-    float hingeSetVal = map(hingeVal, 0, 1023, 0, 180);
-    baseServo.write(hingeSetVal);
-    delay(15);     
-
-    int grabberVal = analogRead(grabberControl);
-    float grabberSetVal = map(grabberVal, 0, 1023, 0, 180);
-    baseServo.write(grabberSetVal);
-    delay(15);
-
-}
-
-// Will be used to control servos based on user input
 char keyboardInput(){
     if(Serial.available() > 0){
         char key_input = Serial.read();
         Serial.println("Input: " + key_input);
         return key_input;
     }
+    return '+';
 }
+
+void loop() {
+
+    if (shouldUseKeyboard){
+        // Keyboard Control
+        char control = keyboardInput();
+        switch(control){
+            // Rotate base hinge forward
+            case 'w':
+                baseHingeCurr--;
+                baseHingeServo.write(baseHingeCurr);
+                delay(15);
+                break;
+
+            // Rotate base hinge backward
+            case 's':
+                baseHingeCurr++;
+                baseHingeServo.write(baseHingeCurr);
+                delay(15);
+                break;
+
+            // Rotate base left
+            case 'a':
+                baseCurr--;
+                baseServo.write(baseCurr);
+                delay(15);
+                break;
+            
+            // Rotate base right
+            case 'd':
+                baseCurr++;
+                baseServo.write(baseCurr);
+                delay(15);
+                break;
+            
+            // Rotate arm hinge forward
+            case 'i':
+                hingeCurr++;
+                hingeServo.write(hingeCurr);
+                delay(15);
+                break;
+
+            // Rotate arm hinge backward
+            case 'k':
+                hingeCurr--;
+                hingeServo.write(hingeCurr);
+                delay(15);
+                break;
+
+            // Toggle grabber
+            case '\n':
+            case '\r':
+                grabberCurr = grabberCurr == 180 ? 0 : 180;
+                grabberServo.write(grabberCurr);
+                delay(15);
+                break;
+            
+            // Ignore if no control inputs are given
+            default:
+                return;
+        }
+    }
+    else{
+        // Control Code
+        int baseVal = analogRead(baseControl);
+        float baseSetVal = map(baseVal, 0, 1023, 0, 180);
+        baseServo.write(baseSetVal);
+        delay(15);     
+
+        int baseHingeVal = analogRead(baseHingeControl);
+        float baseHingeSetVal = map(baseHingeVal, 0, 1023, 0, 180);
+        baseHingeServo.write(baseHingeSetVal);
+        delay(15);     
+
+        int hingeVal = analogRead(hingeControl);
+        float hingeSetVal = map(hingeVal, 0, 1023, 0, 180);
+        baseServo.write(hingeSetVal);
+        delay(15);     
+
+        int grabberVal = analogRead(grabberControl);
+        float grabberSetVal = map(grabberVal, 0, 1023, 0, 180);
+        baseServo.write(grabberSetVal);
+        delay(15);
+    }
+
+}
+
 
 
 
